6a3dca70c4bf146852b1630a958ed8d3818c97ef
itswill00 <anstykx00@gmail.com>
2026-04-15 23:54:29 +0700

    fix: silence tracebacks for MessageIdInvalidError

--- pyUltroid/_misc/_decorators.py
+++ pyUltroid/_misc/_decorators.py
@@ -148,7 +148,7 @@ def ultroid_cmd(
             except (ChatSendInlineForbiddenError, ChatSendMediaForbiddenError, ChatSendStickersForbiddenError, BotMethodInvalidError, UserIsBotError, AlreadyInConversationError, BotInlineDisabledError, DependencyMissingError) as er:
                 return await eod(ult, f"`{er}`" if isinstance(er, (BotInlineDisabledError, DependencyMissingError)) else f"`{str(er)}`")
             except (MessageIdInvalidError, MessageNotModifiedError, MessageDeleteForbiddenError, AuthKeyDuplicatedError) as er:
-                if not isinstance(er, MessageNotModifiedError):
+                if not isinstance(er, (MessageNotModifiedError, MessageIdInvalidError)):
                     LOGS.exception(er)
                 if isinstance(er, AuthKeyDuplicatedError):
                     log_chat = ULT_CONFIG.get("LOG_CHANNEL")
